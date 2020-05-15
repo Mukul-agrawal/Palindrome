65a0b06cfaae425e8741411815bfa021e30c4193
Mukul-agrawal <mukul.agrawal_cs18@gla.ac.in>
2020-05-15 15:25:56 +0530

    create PalindromeFinder class object

--- src/approach1/execution/Main.java
+++ src/approach1/execution/Main.java
@@ -1,10 +1,13 @@
 package approach1.execution;
 
+import approach1.defination.PalindromeFinder;
+
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        PalindromeFinder palindromeFinder = new PalindromeFinder();
         String inputString = scanner.nextLine();
 
 
