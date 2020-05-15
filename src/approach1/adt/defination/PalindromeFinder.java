0586634c17f42745b823922ba68b705ce314c0d9
Mukul-agrawal <mukul.agrawal_cs18@gla.ac.in>
2020-05-15 15:16:26 +0530

    implement method fillStack

--- src/approach1/adt/defination/PalindromeFinder.java
+++ src/approach1/adt/defination/PalindromeFinder.java
@@ -2,12 +2,17 @@ package approach1.adt.defination;
 
 import approach1.adt.PalindromeADT;
 
+import java.util.ArrayDeque;
 import java.util.Deque;
 
 public class PalindromeFinder implements PalindromeADT {
     @Override
     public Deque<Character> fillStack(String inputString) {
-        return null;
+        Deque<Character> charStack = new ArrayDeque<>();
+        for (int i = 0; i < inputString.length(); i++) {
+            charStack.push(inputString.charAt(i));
+        }
+        return charStack;
     }
 
     @Override
