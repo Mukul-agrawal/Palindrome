d861564eb50c094d044d1e4a7a20eae5225df68e
Mukul-agrawal <mukul.agrawal_cs18@gla.ac.in>
2020-05-15 15:31:14 +0530

    approach #1 completed

--- src/approach1/execution/Main.java
+++ src/approach1/execution/Main.java
@@ -8,8 +8,12 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         PalindromeFinder palindromeFinder = new PalindromeFinder();
+        System.out.println("Enter a String :");
         String inputString = scanner.nextLine();
-
-
+        boolean flag = palindromeFinder.isPalindrome(inputString);
+        if (flag)
+            System.out.println("String is Palindrome.");
+        else
+            System.out.println("String is not Palindrome.");
     }
 }
