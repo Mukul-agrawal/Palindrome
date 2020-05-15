9405764ae86616c080982bff20ea730125b76584
Mukul-agrawal <mukul.agrawal_cs18@gla.ac.in>
2020-05-15 16:19:23 +0530

    completed approach #3

--- src/approach3/main/Main.java
+++ src/approach3/main/Main.java
@@ -5,6 +5,14 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a String :");
+        String inputString = scanner.nextLine();
+        StringBuilder sb = new StringBuilder(inputString);
+        if (inputString.equalsIgnoreCase(String.valueOf(sb.reverse())))
+            System.out.println("String is Palindrome.");
+        else
+            System.out.println("String is not Palindrome.");
 
     }
-}
+    }
+
