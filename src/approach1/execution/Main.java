1ad1ed9045b21323d9602b321839a0d8422dbac9
Mukul-agrawal <mukul.agrawal_cs18@gla.ac.in>
2020-05-15 15:21:41 +0530

    create Scanner class object

--- src/approach1/execution/Main.java
+++ src/approach1/execution/Main.java
@@ -1,7 +1,11 @@
 package approach1.execution;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
 
     }
 }
