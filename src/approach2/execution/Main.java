e09621392bac457f4fe1963a77314a6561385dd7
Mukul-agrawal <mukul.agrawal_cs18@gla.ac.in>
2020-05-15 15:49:16 +0530

    create inputString variable

--- src/approach2/execution/Main.java
+++ src/approach2/execution/Main.java
@@ -8,6 +8,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         PalindromeFinder palindromeFinder = new PalindromeFinder();
+        String inputString = scanner.nextLine();
 
 
     }
