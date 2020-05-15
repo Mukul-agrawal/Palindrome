94d56a935c358f5aba6c1f77756cb2389e175d5c
Mukul-agrawal <mukul.agrawal_cs18@gla.ac.in>
2020-05-15 15:42:37 +0530

    create method isPalindrome

--- src/approach2/defination/PalindromeFinder.java
+++ src/approach2/defination/PalindromeFinder.java
@@ -1,4 +1,15 @@
 package approach2.defination;
 
 public class PalindromeFinder {
+    public boolean isPalindrome(String inputString) {
+        boolean response = false;
+        int middle = inputString.length() / 2;
+        for (int i = 0; i < middle; i++) {
+            if (inputString.charAt(i) == inputString.charAt(inputString.length() - 1 - i)) {
+                response = true;
+            }
+            break;
+        }
+        return response;
+    }
 }
