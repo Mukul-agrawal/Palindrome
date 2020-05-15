5a296e4f9ba8330965fc242f425e479f52be915c
Mukul-agrawal <mukul.agrawal_cs18@gla.ac.in>
2020-05-15 15:15:00 +0530

    implement the PalindromeADT interface and override the methods

--- src/approach1/adt/defination/PalindromeFinder.java
+++ src/approach1/adt/defination/PalindromeFinder.java
@@ -1,4 +1,22 @@
 package approach1.adt.defination;
 
-public class PalindromeFinder {
+import approach1.adt.PalindromeADT;
+
+import java.util.Deque;
+
+public class PalindromeFinder implements PalindromeADT {
+    @Override
+    public Deque<Character> fillStack(String inputString) {
+        return null;
+    }
+
+    @Override
+    public String buildReverse(String inputString) {
+        return null;
+    }
+
+    @Override
+    public boolean isPalindrome(String inputString) {
+        return false;
+    }
 }
