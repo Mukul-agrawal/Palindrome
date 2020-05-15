54e73f4ddedd00bec96cd6ba97b711b83e7789f4
Mukul-agrawal <mukul.agrawal_cs18@gla.ac.in>
2020-05-15 15:12:26 +0530

    create abstract methods

--- src/approach1/adt/PalindromeADT.java
+++ src/approach1/adt/PalindromeADT.java
@@ -1,4 +1,11 @@
 package approach1.adt;
 
+import java.util.Deque;
+
 public interface PalindromeADT {
+    Deque<Character> fillStack(String inputString);
+
+    String buildReverse(String inputString);
+
+    boolean isPalindrome(String inputString);
 }
